f3c48fd8ce5ae1943ae53048d989a1c6c8a15f81
tayseer2 <tysyrt200@gmail.com>
2024-08-10 23:48:44 +0300

    Added the loder

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,94 @@
+// import Hero from "./components/2-hero/Hero";
+// import Header from "./components/1-header/Header";
+// import Main from "./components/3-main/Main";
+// import Contact from "./components/4-contact/Contact";
+// import Footer from "./components/5-footer/Footer";
+// import { useEffect, useState } from "react";
+
+// function App() {
+//   useEffect(() => {
+//     window.addEventListener("scroll", () => {
+//       if (window.scrollY > 300) {
+//         setShowBtn(true);
+//       } else {
+//         setShowBtn(false);
+//       }
+//     });
+//   }, []);
+//   const [showBtn, setShowBtn] = useState(false);
+//   return (
+//     <div className="container">
+//       <Header />
+//       <Hero />
+//       <div className="divider" />
+//       <Main />
+//       <div className="divider" />
+//       <Contact />
+//       <div className="divider" />
+//       <Footer />
+
+//       <button
+//         onClick={() => {
+//           window.scrollTo({
+//             top: 0,
+//           });
+//         }}
+//         style={{ opacity: showBtn ? "1" : "0" }}
+//         className="scroll2Top icon-keyboard_arrow_up"
+//       ></button>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import  { useEffect, useState } from "react";
 import Hero from "./components/2-hero/Hero";
 import Header from "./components/1-header/Header";
 import Main from "./components/3-main/Main";
 import Contact from "./components/4-contact/Contact";
 import Footer from "./components/5-footer/Footer";
-import { useEffect, useState } from "react";
+import "./components/Spinner/Spinner";
+
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState(null);
+  const [showBtn, setShowBtn] = useState(false);
+
   useEffect(() => {
-    window.addEventListener("scroll", () => {
+    setTimeout(() => {
+      setData({ message: "Data loaded!" });
+      setLoading(false);
+    }, 3000); 
+  }, []);
+
+  useEffect(() => {
+    const handleScroll = () => {
       if (window.scrollY > 300) {
         setShowBtn(true);
       } else {
         setShowBtn(false);
       }
-    });
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
   }, []);
-  const [showBtn, setShowBtn] = useState(false);
+
+  if (loading) {
+    return (
+      <div className="spinner-container">
+        <div className="spinner"></div>
+      </div>
+    );
+  }
+
   return (
     <div className="container">
-      <Header  />
+      <Header />
       <Hero />
       <div className="divider" />
       <Main />
