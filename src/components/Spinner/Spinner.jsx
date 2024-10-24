c5f262f8fe3a20722ee452b8728b3e7ecd75e77d
tayseer2 <tysyrt200@gmail.com>
2024-10-24 19:39:55 +0300

    Edit On loader

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -3,80 +3,13 @@ import "./spinner.css";
 const Spinner = () => {
   return (
     <div className="loader-container">
-      <div className="my-loader">
-        <div className="rubiks-cube">
-          <div className="face front">
-            <div style={{ background: "#ff3d00" }} className="cube"></div>
-            <div style={{ background: "#ffeb3b" }} className="cube"></div>
-            <div style={{ background: "#4caf50" }} className="cube"></div>
-            <div style={{ background: "#2196f3" }} className="cube"></div>
-            <div style={{ background: "#ffffff" }} className="cube"></div>
-            <div style={{ background: "#ffeb3b" }} className="cube"></div>
-            <div style={{ background: "#4caf50" }} className="cube"></div>
-            <div style={{ background: "#2196f3" }} className="cube"></div>
-            <div style={{ background: "#ff3d00" }} className="cube"></div>
-          </div>
-
-          <div className="face back">
-            <div style={{ background: "#4caf50" }} className="cube"></div>
-            <div style={{ background: "#ff3d00" }} className="cube"></div>
-            <div style={{ background: "#ffeb3b" }} className="cube"></div>
-            <div style={{ background: "#2196f3" }} className="cube"></div>
-            <div style={{ background: "#ffffff" }} className="cube"></div>
-            <div style={{ background: "#ff3d00" }} className="cube"></div>
-            <div style={{ background: "#ffeb3b" }} className="cube"></div>
-            <div style={{ background: "#4caf50" }} className="cube"></div>
-            <div style={{ background: "#2196f3" }} className="cube"></div>
-          </div>
-
-          <div className="face left">
-            <div style={{ background: "#ffeb3b" }} className="cube"></div>
-            <div style={{ background: "#4caf50" }} className="cube"></div>
-            <div style={{ background: "#2196f3" }} className="cube"></div>
-            <div style={{ background: "#ff3d00" }} className="cube"></div>
-            <div style={{ background: "#ffffff" }} className="cube"></div>
-            <div style={{ background: "#4caf50" }} className="cube"></div>
-            <div style={{ background: "#2196f3" }} className="cube"></div>
-            <div style={{ background: "#ffeb3b" }} className="cube"></div>
-            <div style={{ background: "#ff3d00" }} className="cube"></div>
-          </div>
-
-          <div className="face right">
-            <div style={{ background: "#4caf50" }} className="cube"></div>
-            <div style={{ background: "#ff3d00" }} className="cube"></div>
-            <div style={{ background: "#ffeb3b" }} className="cube"></div>
-            <div style={{ background: "#2196f3" }} className="cube"></div>
-            <div style={{ background: "#ffffff" }} className="cube"></div>
-            <div style={{ background: "#ff3d00" }} className="cube"></div>
-            <div style={{ background: "#ffeb3b" }} className="cube"></div>
-            <div style={{ background: "#4caf50" }} className="cube"></div>
-            <div style={{ background: "#2196f3" }} className="cube"></div>
-          </div>
-
-          <div className="face top">
-            <div style={{ background: "#2196f3" }} className="cube"></div>
-            <div style={{ background: "#ffeb3b" }} className="cube"></div>
-            <div style={{ background: "#ff3d00" }} className="cube"></div>
-            <div style={{ background: "#4caf50" }} className="cube"></div>
-            <div style={{ background: "#ffffff" }} className="cube"></div>
-            <div style={{ background: "#ffeb3b" }} className="cube"></div>
-            <div style={{ background: "#ff3d00" }} className="cube"></div>
-            <div style={{ background: "#4caf50" }} className="cube"></div>
-            <div style={{ background: "#2196f3" }} className="cube"></div>
-          </div>
-
-          <div className="face bottom">
-            <div style={{ background: "#ffffff" }} className="cube"></div>
-            <div style={{ background: "#4caf50" }} className="cube"></div>
-            <div style={{ background: "#2196f3" }} className="cube"></div>
-            <div style={{ background: "#ff3d00" }} className="cube"></div>
-            <div style={{ background: "#ffeb3b" }} className="cube"></div>
-            <div style={{ background: "#4caf50" }} className="cube"></div>
-            <div style={{ background: "#2196f3" }} className="cube"></div>
-            <div style={{ background: "#ffffff" }} className="cube"></div>
-            <div style={{ background: "#ff3d00" }} className="cube"></div>
-          </div>
-        </div>
+      <div className="spinner">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
       </div>
     </div>
   );
